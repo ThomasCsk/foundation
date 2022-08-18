9548a37e673cc442161dc2e3b99a197f4acfe903
Thomas Ciszek <tciszek515@gmail.com>
2022-08-18 19:32:28 -0400

    models created

--- server/models/Application.js
+++ server/models/Application.js
@@ -1,11 +1,84 @@
-import { Schema, model } from "mongoose";
+import { Schema } from "mongoose";
+
+const dateFormat = (timestamp) => {
+  const day = new Date(timestamp)
+  return (day.getMonth()+1) + 
+          '/' +
+          day.getDate() + 
+          '/' + 
+          day.getFullYear() +
+          'at' +
+          day.getHours() + 
+          ':' + 
+          day.getMinutes();
+}
 
 const applicationSchema = new Schema(
   {
-
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    birthday: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      match: [/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/, 'Must be a valid phone number!']
+    },
+    addressStreet: {
+      type: String,
+      required: true
+    },
+    addressCity: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    zipCode: {
+      type: Number,
+      required: true
+    },
+    guardianName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    guardianPhone: {
+      type: String,
+      required: true,
+      match: [/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/, 'Must be a valid phone number!']
+    },
+    grade: {
+      type: String,
+      required: true
+    },
+    school: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    currentStatus: {
+      // 0 for pending, 1 for accepted, 2 for denied
+      type: Number,
+      required: true
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
   }
 )
 
-const Application = model('Application', applicationSchema);
-
-export default Application;
+export default applicationSchema;
--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,57 @@
 import { Schema, model } from "mongoose";
 import bcrypt from 'bcrypt';
 
+import applicationSchema from './Application'
+
 const userSchema = new Schema(
   {
-
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    admin: {
+      // 0 if student, 1 if staff/admin
+      type: Boolean,
+      required: true
+    },
+    applications: [applicationSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
   }
 )
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema)
 
