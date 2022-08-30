ff1fb0044e32a2bbec44c57ccaa7ff369c49ca26
Thomas Ciszek <tciszek515@gmail.com>
2022-08-29 20:42:08 -0400

    init files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import LogInSignUp from './pages/LoginSignUp'
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Header/>
+          <div>
+            <Routes>
+              <Route
+                path="/"
+                element={<Home/>}
+              />
+              <Route
+                path="/dashboard"
+                element={<Dashboard/>}
+              />
+              <Route
+                path="/loginsignup"
+                element={<LogInSignUp/>}
+              />
+            </Routes>
+          </div>
+          <Footer/>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/pages/LoginSignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginSignUp = () => {
+  return(
+    <div>
+      <div>LogIn</div>
+      <div>SignUp</div>
+    </div>
+  )
+};
+
+export default LoginSignUp;
